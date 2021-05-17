348a0a0712811e5d50a8fefd8f5262b73fa7086b
edanlx <876651109@qq.com>
2021-05-17 20:16:57 +0800

    guava

---
+++ demoGrace/src/main/java/com/example/demo/lesson/grace/guava/GuavaExample.java
@@ -0,0 +1,137 @@
+package com.example.demo.lesson.grace.guava;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Table;
+import org.springframework.util.StopWatch;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class GuavaExample {
+    public static void main(String[] args) {
+        // immutableOrdinary();
+        // effectiveList();
+        newCollections();
+    }
+
+    public static void newCollections(){
+        // 这里只介绍BiMap和Table。Multiset、Multimap这两个就是嵌了list进去
+        BiMap<Integer,String> biMap=HashBiMap.create();
+        biMap.put(1,"张三");
+        biMap.put(2,"李四");
+        biMap.put(3,"王五");
+        biMap.put(4,"赵六");
+        biMap.put(5,"李七");
+        biMap.put(6,"小小");
+        Integer result = biMap.inverse().get("赵六");
+        // 输出结果4
+        System.out.println(result);
+        // ===========================================================
+        // table是个很有意思的数据结构，很有启发性思维，虽然我也不知道这个有啥用
+        /*
+         *  Company: IBM, Microsoft, TCS
+         *  IBM         -> {101:Mahesh, 102:Ramesh, 103:Suresh}
+         *  Microsoft     -> {101:Sohan, 102:Mohan, 103:Rohan }
+         *  TCS         -> {101:Ram, 102: Shyam, 103: Sunil }
+         *
+         * */
+        //create a table
+        Table<String, String, String> employeeTable = HashBasedTable.create();
+
+        //initialize the table with employee details
+        employeeTable.put("IBM", "101","Mahesh");
+        employeeTable.put("IBM", "102","Ramesh");
+        employeeTable.put("IBM", "103","Suresh");
+
+        employeeTable.put("Microsoft", "111","Sohan");
+        employeeTable.put("Microsoft", "112","Mohan");
+        employeeTable.put("Microsoft", "113","Rohan");
+
+        employeeTable.put("TCS", "121","Ram");
+        employeeTable.put("TCS", "102","Shyam");
+        employeeTable.put("TCS", "123","Sunil");
+
+        //所有行数据
+        System.out.println(employeeTable.cellSet());
+        //所有公司
+        System.out.println(employeeTable.rowKeySet());
+        //所有员工编号
+        System.out.println(employeeTable.columnKeySet());
+        //所有员工名称
+        System.out.println(employeeTable.values());
+        //公司中的所有员工和员工编号
+        System.out.println(employeeTable.rowMap());
+        //员工编号对应的公司和员工名称
+        System.out.println(employeeTable.columnMap());
+    }
+
+    public static void effectiveList() {
+        StopWatch sw = new StopWatch();
+        // sw.start("listAdd");
+        Stream.Builder<Integer> builder1 = Stream.builder();
+        for (int i = 0; i < 10000; i++) {
+            builder1.add(i);
+        }
+        List<Integer> list = builder1.build().collect(Collectors.toList());
+        // sw.stop();
+
+        // sw.start("ImmutableListAdd");
+        ImmutableList.Builder<Integer> builder = ImmutableList.builder();
+        for (int i = 0; i < 10000; i++) {
+            builder.add(i);
+        }
+        List<Integer> guava = builder.build();
+        // sw.stop();
+
+        List<Integer> listUn = Collections.unmodifiableList(list);
+//        sw.start("listGet");
+//        list.get(0);
+//        sw.stop();
+//        sw.start("listUnGet");
+//        listUn.get(0);
+//        sw.stop();
+//        sw.start("guavaGet");
+//        guava.get(0);
+//        sw.stop();
+
+        sw.start("listFor");
+        IntStream.range(0, 10000).boxed().forEach(list::get);
+        sw.stop();
+        sw.start("listUnFor");
+        IntStream.range(0, 10000).boxed().forEach(listUn::get);
+        sw.stop();
+        sw.start("guavaFor");
+        IntStream.range(0, 10000).boxed().forEach(guava::get);
+        sw.stop();
+        System.out.println(sw.prettyPrint());
+    }
+
+    public static void immutableOrdinary() {
+        // 常规创建
+        Set<Integer> set = ImmutableSet.of(1, 2, 3, 1);
+        List<Integer> list = ImmutableList.of(1, 2, 3, 1);
+        // map的k-v连续的写法还是非常舒服的
+        Map<Integer, Integer> map = ImmutableMap.of(1, 2, 3, 1);
+
+        // 循环创建
+        ImmutableSet.Builder<Integer> builder = ImmutableSet.<Integer>builder();
+        for (int i = 0; i < 10; i++) {
+            builder.add(i);
+        }
+        Set<Integer> build = builder.build();
+
+        // 常规list转不可变,值得注意的是Collections.unmodifiableList()如果原list被改变不可变是会被改变的，guava的是深拷贝
+        List<Integer> collect = Stream.of(1, 2, 3, 4).collect(Collectors.toList());
+        List<Integer> integers = ImmutableList.copyOf(collect);
+    }
+}
