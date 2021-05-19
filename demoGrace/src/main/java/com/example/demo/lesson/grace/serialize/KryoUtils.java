4317dc259f8dfccab756cbd6ca4e62a9d20b5cd8
edanlx <876651109@qq.com>
2021-05-19 11:36:45 +0800

    Serial

--- demoGrace/src/main/java/com/example/demo/lesson/grace/serialize/KryoUtils.java
+++ demoGrace/src/main/java/com/example/demo/lesson/grace/serialize/KryoUtils.java
@@ -3,36 +3,43 @@ package com.example.demo.lesson.grace.serialize;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
-import lombok.Cleanup;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.Serializable;
 
 public class KryoUtils {
-    public static Kryo KRYO = new Kryo();
+    // 这个东西线程不安全
+    private static final ThreadLocal<Kryo> KRYO = ThreadLocal.withInitial(() -> {
+        Kryo kryo = new Kryo();
+        kryo.setInstantiatorStrategy(new Kryo.DefaultInstantiatorStrategy());
+        return kryo;
+    });
 
+    //    newKryoPool() {
+//        return new KryoPool.Builder(() -> {
+//            final Kryo kryo = new Kryo();
+//            kryo.setInstantiatorStrategy(new Kryo.DefaultInstantiatorStrategy(
+//                    new StdInstantiatorStrategy()));
+//            return kryo;
+//        }).softReferences().build();
+//    }
     //    https://www.jianshu.com/p/f56c9360936d
     public static <T extends Serializable> byte[] serialization(T obj) {
-        KRYO.register(obj.getClass());
-        try {
-            @Cleanup ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            @Cleanup Output output = new Output(baos);
-            KRYO.writeClassAndObject(output, obj);
-            return baos.toByteArray();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        KRYO.get().register(obj.getClass());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Output output = new Output(baos);
+        // KRYO.get().writeClassAndObject(output, obj);
+        KRYO.get().writeObject(output, obj);
+        output.close();
+        return baos.toByteArray();
     }
 
     public static <T extends Serializable> T deserialization(byte[] b, Class<T> clazz) {
-        KRYO.register(clazz);
-        try {
-            @Cleanup ByteArrayInputStream bais = new ByteArrayInputStream(b);
-            @Cleanup Input input = new Input(bais);
-            return (T) KRYO.readClassAndObject(input);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        KRYO.get().register(clazz);
+        ByteArrayInputStream bais = new ByteArrayInputStream(b);
+        Input input = new Input(bais);
+        // return (T) KRYO.get().readClassAndObject(input);
+        return (T) KRYO.get().readObject(input, clazz);
     }
 }
--- demoGrace/src/main/java/com/example/demo/lesson/grace/serialize/SerializeExample.java
+++ demoGrace/src/main/java/com/example/demo/lesson/grace/serialize/SerializeExample.java
@@ -1,31 +1,147 @@
 package com.example.demo.lesson.grace.serialize;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.Data;
+import lombok.SneakyThrows;
 import lombok.ToString;
 import org.apache.commons.lang3.SerializationUtils;
+import org.springframework.util.StopWatch;
 
 import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 
 public class SerializeExample {
     @Data
     @ToString
-    public static class SerializeTestObject implements Serializable{
+    public static class SerializeTestObject implements Serializable {
         private String name;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
+        // compareLength();
+//        compareSerialize();
+         compareDeSerialize();
+    }
+
+    @SneakyThrows
+    private static void compareDeSerialize() {
+        SerializeTestObject serializeTestObject = new SerializeTestObject();
+        serializeTestObject.name = "1";
+        String jsonStr = JSON.toJSONString(serializeTestObject);
+        byte[] serialize = SerializationUtils.serialize(serializeTestObject);
+        byte[] serializeHe = Hessian2Utils.serialize(serializeTestObject);
+        byte[] serializeHeKryo = KryoUtils.serialization(serializeTestObject);
+        TypeReference<SerializeTestObject> fastJsonType = new TypeReference<SerializeTestObject>() {
+        };
+        com.fasterxml.jackson.core.type.TypeReference<SerializeTestObject> jackJsonType = new com.fasterxml.jackson.core.type.TypeReference<SerializeTestObject>() {
+        };
+        Type gsonType = new TypeToken<SerializeTestObject>() {
+        }.getType();
+        StopWatch sw = new StopWatch();
+        sw.start("serialize");
+        for (int i = 0; i < 10000; i++) {
+            SerializeTestObject deserialize = SerializationUtils.<SerializeTestObject>deserialize(serialize);
+        }
+        sw.stop();
+        sw.start("He");
+        for (int i = 0; i < 10000; i++) {
+            SerializeTestObject deserializeHe = Hessian2Utils.<SerializeTestObject>deserialize(serializeHe);
+        }
+        sw.stop();
+        sw.start("Kryo");
+        for (int i = 0; i < 10000; i++) {
+            SerializeTestObject deserializeKryo = KryoUtils.deserialization(serializeHeKryo, SerializeTestObject.class);
+        }
+        sw.stop();
+        sw.start("fastJson");
+        for (int i = 0; i < 10000; i++) {
+            SerializeTestObject fastJsonObject = JSON.parseObject(jsonStr, fastJsonType);
+        }
+        sw.stop();
+        sw.start("jackson");
+        // 线程安全
+        ObjectMapper objectMapper = new ObjectMapper();
+        for (int i = 0; i < 10000; i++) {
+            SerializeTestObject jacksonObject = objectMapper.readValue(jsonStr, jackJsonType);
+        }
+        sw.stop();
+        // 线程安全
+        Gson gson = new Gson();
+        sw.start("Gson");
+        for (int i = 0; i < 10000; i++) {
+            SerializeTestObject gsonObject = gson.fromJson(jsonStr, gsonType);
+        }
+        sw.stop();
+        System.out.println(sw.prettyPrint());
+    }
+
+    @SneakyThrows
+    public static void compareSerialize() {
+        SerializeTestObject serializeTestObject = new SerializeTestObject();
+        serializeTestObject.name = "1";
+        StopWatch sw = new StopWatch();
+        sw.start("serialize");
+        for (int i = 0; i < 10000; i++) {
+            SerializationUtils.serialize(serializeTestObject);
+        }
+        sw.stop();
+        sw.start("He");
+        for (int i = 0; i < 10000; i++) {
+            Hessian2Utils.serialize(serializeTestObject);
+        }
+        sw.stop();
+        sw.start("Kryo");
+        for (int i = 0; i < 10000; i++) {
+            KryoUtils.serialization(serializeTestObject);
+        }
+        sw.stop();
+        sw.start("fastJson");
+        for (int i = 0; i < 10000; i++) {
+            JSON.toJSONString(serializeTestObject);
+        }
+        sw.stop();
+        sw.start("jackson");
+        // 线程安全
+        ObjectMapper objectMapper = new ObjectMapper();
+        for (int i = 0; i < 10000; i++) {
+            objectMapper.writeValueAsString(serializeTestObject);
+        }
+        sw.stop();
+        // 线程安全
+        Gson gson = new Gson();
+        sw.start("Gson");
+        for (int i = 0; i < 10000; i++) {
+            gson.toJson(serializeTestObject);
+        }
+        sw.stop();
+        System.out.println(sw.prettyPrint());
+    }
+
+    public static void compareLength() {
+        // 输出129，88，4，12,可以看出json格式的优秀，还有Kryo针对java格式的优秀
         SerializeTestObject serializeTestObject = new SerializeTestObject();
         serializeTestObject.name = "1";
         byte[] serialize = SerializationUtils.serialize(serializeTestObject);
         SerializeTestObject deserialize = SerializationUtils.<SerializeTestObject>deserialize(serialize);
         System.out.println(deserialize);
+        System.out.println(serialize.length);
 
         byte[] serializeHe = Hessian2Utils.serialize(serializeTestObject);
         SerializeTestObject deserializeHe = Hessian2Utils.<SerializeTestObject>deserialize(serializeHe);
         System.out.println(deserializeHe);
+        System.out.println(serializeHe.length);
 
         byte[] serializeHeKryo = KryoUtils.serialization(serializeTestObject);
         SerializeTestObject deserializeKryo = KryoUtils.deserialization(serializeHeKryo, SerializeTestObject.class);
         System.out.println(deserializeKryo);
+        System.out.println(serializeHeKryo.length);
+
+        System.out.println(JSON.toJSONString(serializeTestObject).getBytes(StandardCharsets.UTF_8).length);
     }
 }
