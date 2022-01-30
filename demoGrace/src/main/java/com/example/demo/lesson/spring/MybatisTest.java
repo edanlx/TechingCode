648579cbb91195c028cac88a59e12cad33d4e1ba
edanlx <876651109@qq.com>
2022-01-30 16:44:18 +0800

    update

---
+++ demoGrace/src/main/java/com/example/demo/lesson/spring/MybatisTest.java
@@ -0,0 +1,8 @@
+package com.example.demo.lesson.spring;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+
+@MapperScan
+public class MybatisTest {
+}
---
+++ demoGrace/src/main/java/com/example/demo/lesson/spring/Spring.java
@@ -0,0 +1,9 @@
+package com.example.demo.lesson.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Spring {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Spring.class);
+    }
+}
